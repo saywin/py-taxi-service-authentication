1dd9666ef150c4371d479748558a632addf60048
Artem <stilon670@gmail.com>
2025-10-04 11:49:00 +0300

    Implement count visits to index page

--- taxi/views.py
+++ taxi/views.py
@@ -10,11 +10,14 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
+    num_visits = request.session.get("num_visits", 0) + 1
+    request.session["num_visits"] = num_visits
 
     context = {
         "num_drivers": num_drivers,
         "num_cars": num_cars,
         "num_manufacturers": num_manufacturers,
+        "num_visits": num_visits,
     }
 
     return render(request, "taxi/index.html", context=context)
